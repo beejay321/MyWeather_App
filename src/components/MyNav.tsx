e77a47e80bd71d9c71a3e65529ae24ab16f85759
beejay321 <jibodu.busola@gmail.com>
2021-11-15 14:38:56 +0100

    Update MyNav.tsx

--- src/components/MyNav.tsx
+++ src/components/MyNav.tsx
@@ -19,13 +19,13 @@ const MyNav = ({ title, location }: NavProps) => (
               <div className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</div>
             </Link>
 
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
           <Form>{/* <Button variant="outline-success">Search</Button> */}</Form>
         </Navbar.Collapse>
